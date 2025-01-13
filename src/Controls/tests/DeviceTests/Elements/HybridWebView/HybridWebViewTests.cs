d6129853674f928e974069f6116a505c3be4a6f4
Matthew Leibowitz <mattleibow@live.com>
2025-01-13 21:17:40 +0800

    test a new way

--- src/Controls/tests/DeviceTests/Elements/HybridWebView/HybridWebViewTests.cs
+++ src/Controls/tests/DeviceTests/Elements/HybridWebView/HybridWebViewTests.cs
@@ -171,6 +171,28 @@ namespace Microsoft.Maui.DeviceTests
 			});
 
 		[Fact]
+		public Task InvokeJavaScriptMethodWithParametersAndVoidReturnUsingNullReturnMethod() =>
+			RunTest(async (hybridWebView) =>
+			{
+				var x = 123.456m;
+				var y = 654.321m;
+
+				var firstResult = await hybridWebView.InvokeJavaScriptAsync<object>(
+					"EvaluateMeWithParamsAndVoidReturn",
+					null,
+					[x, y],
+					[HybridWebViewTestContext.Default.Decimal, HybridWebViewTestContext.Default.Decimal]);
+
+				Assert.Null(firstResult);
+
+				var result = await hybridWebView.InvokeJavaScriptAsync<decimal>(
+					"EvaluateMeWithParamsAndVoidReturnGetResult",
+					HybridWebViewTestContext.Default.Decimal);
+
+				Assert.Equal(777.777m, result);
+			});
+
+		[Fact]
 		public Task EvaluateJavaScriptAndGetResult() =>
 			RunTest(async (hybridWebView) =>
 			{
